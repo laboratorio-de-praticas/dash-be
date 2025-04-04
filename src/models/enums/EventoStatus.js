ee407ba6c9055da99a634739db2e1d4e51813fbe
SouzaDiegoCl <souzadiegocl@gmail.com>
2025-04-04 20:26:55 -0300

    feat: Criação dos enums

---
+++ src/models/enums/EventoStatus.js
@@ -0,0 +1,9 @@
+const EventoStatus = {
+  Ativo: "Ativo",
+  Em_Preparo: "Em_Preparo",
+  Em_Preparo: "Em_Preparo",
+  Em_Contagem: "Em_Contagem",
+  Finalizado: "Finalizado",
+};
+
+export default EventoStatus;
